8a00f5c6179cfc8fd1c9064878de8cbc1fc8cda0
iloktionov <iloktionov@skbkontur.ru>
2018-07-20 12:39:38 +0500

    A couple of unit tests for LogEventExtensions.

---
+++ Vostok.Logging.Abstractions.Tests/LogEventExtensions_Tests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Vostok.Logging.Abstractions.Tests
+{
+    [TestFixture]
+    internal class LogEventExtensions_Tests
+    {
+        private LogEvent eventBefore;
+        private LogEvent eventAfter;
+
+        [SetUp]
+        public void TestSetup()
+        {
+            eventBefore = new LogEvent(LogLevel.Info, DateTimeOffset.UtcNow, null)
+                .WithProperty("A", 1)
+                .WithProperty("B", 2);
+        }
+
+        [Test]
+        public void WithParameters_should_return_same_event_when_params_array_is_null()
+        {
+            eventAfter = eventBefore.WithParameters(null);
+
+            eventAfter.Should().BeSameAs(eventBefore);
+        }
+
+        [Test]
+        public void WithParameters_should_enrich_event_with_index_named_properties()
+        {
+            eventAfter = eventBefore.WithParameters(new object[] {"value", null, 123});
+
+            eventAfter.Properties.Should().HaveCount(5);
+            eventAfter?.Properties?["0"].Should().Be("value");
+            eventAfter?.Properties?["1"].Should().BeNull();
+            eventAfter?.Properties?["2"].Should().Be(123);
+        }
+    }
+}
