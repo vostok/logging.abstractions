87869eeca97f7ac8de9f780c3df39349ebb3cb0f
iloktionov <iloktionov@skbkontur.ru>
2018-07-29 15:07:15 +0500

    No more flashing our guts to Vostok.Logging.Core

--- Vostok.Logging.Abstractions/Properties/AssemblyInfo.cs
+++ Vostok.Logging.Abstractions/Properties/AssemblyInfo.cs
@@ -1,6 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("Vostok.Logging.Core")]
-[assembly: InternalsVisibleTo("Vostok.Logging.Tests")]
 [assembly: InternalsVisibleTo("Vostok.Logging.Abstractions.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
