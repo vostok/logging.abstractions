f664646e4c453f42904a4f3e548fd69fe8798475
iloktionov <iloktionov@skbkontur.ru>
2018-08-07 14:51:02 +0500

    Added consts with the names of some well-known LogEvent properties.

---
+++ Vostok.Logging.Abstractions/WellKnownProperties.cs
@@ -0,0 +1,15 @@
+﻿using JetBrains.Annotations;
+
+namespace Vostok.Logging.Abstractions
+{
+    /// <summary>
+    /// Defines names of special well-known properties in <see cref="LogEvent"/>s.
+    /// </summary>
+    [PublicAPI]
+    public static class WellKnownProperties
+    {
+        public const string SourceContext = "SourceContext";
+
+        public const string ContextualPrefix = "ContextualPrefix";
+    }
+}
