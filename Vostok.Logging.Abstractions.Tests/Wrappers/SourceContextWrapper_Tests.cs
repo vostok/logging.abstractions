e0f0f522a1958d042d8d9addd10abb5990482d72
iloktionov <iloktionov@skbkontur.ru>
2018-08-16 16:50:35 +0500

    Added a helper ILog wrapper to facilitate implementation of ForContext() in custom logs.

---
+++ Vostok.Logging.Abstractions.Tests/Wrappers/SourceContextWrapper_Tests.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Linq;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+using Vostok.Logging.Abstractions.Wrappers;
+
+namespace Vostok.Logging.Abstractions.Tests.Wrappers
+{
+    [TestFixture]
+    internal class SourceContextWrapper_Tests
+    {
+        private ILog baseLog;
+        private ILog wrapper;
+        private LogEvent originalEvent;
+        private LogEvent observedEvent;
+
+        [SetUp]
+        public void TestSetup()
+        {
+            baseLog = Substitute.For<ILog>();
+            baseLog.When(log => log.Log(Arg.Any<LogEvent>())).Do(info => observedEvent = info.Arg<LogEvent>());
+
+            originalEvent = new LogEvent(LogLevel.Info, DateTimeOffset.Now, null)
+                .WithProperty("name1", "value1")
+                .WithProperty("name2", "value2");
+
+            observedEvent = null;
+
+            wrapper = new SourceContextWrapper(baseLog, "ctx-value");
+        }
+
+        [Test]
+        public void Log_method_should_handle_null_events()
+        {
+            wrapper.Log(null);
+
+            baseLog.Received().Log(null);
+        }
+
+        [Test]
+        public void Log_method_should_enrich_events_with_source_context_property()
+        {
+            wrapper.Log(originalEvent);
+
+            baseLog.Received(1).Log(Arg.Any<LogEvent>());
+
+            observedEvent.Properties.Should().HaveCount(3);
+            observedEvent.Properties?[WellKnownProperties.SourceContext].Should().Be("ctx-value");
+        }
+
+        [Test]
+        public void IsEnabled_method_should_delegate_to_base_log()
+        {
+            foreach (var level in Enum.GetValues(typeof(LogLevel)).Cast<LogLevel>())
+            {
+                baseLog.IsEnabledFor(level).Returns(true);
+
+                wrapper.IsEnabledFor(level).Should().BeTrue();
+
+                baseLog.IsEnabledFor(level).Returns(false);
+
+                wrapper.IsEnabledFor(level).Should().BeFalse();
+            }
+        }
+
+        [Test]
+        public void ForContext_with_null_argument_should_return_base_log()
+        {
+            wrapper.ForContext(null).Should().BeSameAs(baseLog);
+        }
+
+        [Test]
+        public void ForContext_should_not_produce_wrapper_chains()
+        {
+            wrapper = wrapper
+                .ForContext("1")
+                .ForContext("2")
+                .ForContext("3");
+
+            var result = wrapper.Should().BeOfType<SourceContextWrapper>().Which;
+
+            result.BaseLog.Should().BeSameAs(baseLog);
+
+            result.Context.Should().Be("3");
+        }
+
+        [Test]
+        public void ForContext_should_unwrap_chains_of_source_context_wrappers()
+        {
+            wrapper = new SourceContextWrapper(wrapper, "1");
+            wrapper = new SourceContextWrapper(wrapper, "2");
+            wrapper = new SourceContextWrapper(wrapper, "3");
+            wrapper = wrapper.ForContext("4");
+
+            var result = wrapper.Should().BeOfType<SourceContextWrapper>().Which;
+
+            result.BaseLog.Should().BeSameAs(baseLog);
+
+            result.Context.Should().Be("4");
+        }
+    }
+}
---
+++ Vostok.Logging.Abstractions/Wrappers/SourceContextWrapper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace Vostok.Logging.Abstractions.Wrappers
+{
+    /// <summary>
+    /// <para>An <see cref="ILog"/> wrapper that helps to implement <see cref="ILog.ForContext"/> method in custom logs efficiently.</para>
+    /// <para><see cref="SourceContextWrapper"/> enriches all passing events with a <see cref="WellKnownProperties.SourceContext"/> property with given value.</para>
+    /// <para>It's <see cref="ILog.ForContext"/> implementation prevents formation of wrapper chains by unwrapping to a base log that is not a <see cref="SourceContextWrapper"/>.</para>
+    /// </summary>
+    [PublicAPI]
+    public class SourceContextWrapper : ILog
+    {
+        public SourceContextWrapper([NotNull] ILog log, [NotNull] string context)
+        {
+            BaseLog = log ?? throw new ArgumentNullException(nameof(log));
+            Context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        [NotNull]
+        public ILog BaseLog { get; }
+
+        [NotNull]
+        public string Context { get; }
+
+        public void Log(LogEvent @event)
+        {
+            @event = @event?.WithProperty(WellKnownProperties.SourceContext, Context, true);
+
+            BaseLog.Log(@event);
+        }
+
+        public bool IsEnabledFor(LogLevel level)
+        {
+            return BaseLog.IsEnabledFor(level);
+        }
+
+        public ILog ForContext(string context)
+        {
+            if (context == null)
+                return UnwrapBaseLog();
+
+            return new SourceContextWrapper(UnwrapBaseLog(), context);
+        }
+
+        private ILog UnwrapBaseLog()
+        {
+            var result = BaseLog;
+
+            while (result is SourceContextWrapper wrapper)
+            {
+                result = wrapper.BaseLog;
+            }
+
+            return result;
+        }
+    }
+}
