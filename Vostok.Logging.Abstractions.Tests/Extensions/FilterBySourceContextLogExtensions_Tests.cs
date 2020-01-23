9557a165ce61d2c452bb7dcfe3a01f3c6cc369f6
iloktionov <iloktionov@skbkontur.ru>
2020-01-23 13:21:16 +0500

    Implemented https://github.com/vostok/logging.abstractions/issues/8

--- Vostok.Logging.Abstractions.Tests/Extensions/FilterBySourceContextLogExtensions_Tests.cs
+++ Vostok.Logging.Abstractions.Tests/Extensions/FilterBySourceContextLogExtensions_Tests.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Linq;
+using FluentAssertions;
 using NSubstitute;
 using NUnit.Framework;
 
@@ -18,6 +20,7 @@ namespace Vostok.Logging.Abstractions.Tests.Extensions
         {
             baseLog = Substitute.For<ILog>();
             baseLog.ForContext(Arg.Any<string>()).Returns(info => baseLog);
+            baseLog.IsEnabledFor(Arg.Any<LogLevel>()).Returns(true);
 
             @event = new LogEvent(LogLevel.Info, DateTimeOffset.Now, null);
             context = "TestContext";
@@ -25,7 +28,7 @@ namespace Vostok.Logging.Abstractions.Tests.Extensions
         }
 
         [Test]
-        public void WithEventsSelectedBySourceContext_should_return_a_log_that_select_events_with_context()
+        public void WithEventsSelectedBySourceContext_should_return_a_log_that_selects_events_with_context()
         {
             var filterLog = baseLog.WithEventsSelectedBySourceContext(context);
 
@@ -37,7 +40,7 @@ namespace Vostok.Logging.Abstractions.Tests.Extensions
         }
 
         [Test]
-        public void WithEventsSelectedBySourceContext_should_return_a_log_that_drop_events_without_context()
+        public void WithEventsSelectedBySourceContext_should_return_a_log_that_drops_events_without_context()
         {
             var filterLog = baseLog.WithEventsSelectedBySourceContext(context);
 
@@ -48,7 +51,7 @@ namespace Vostok.Logging.Abstractions.Tests.Extensions
         }
 
         [Test]
-        public void WithEventsDroppedBySourceContext_should_return_a_log_that_drop_events_with_context()
+        public void WithEventsDroppedBySourceContext_should_return_a_log_that_drops_events_with_context()
         {
             var filterLog = baseLog.WithEventsDroppedBySourceContext(context);
 
@@ -60,7 +63,7 @@ namespace Vostok.Logging.Abstractions.Tests.Extensions
         }
 
         [Test]
-        public void WithEventsDroppedBySourceContext_should_return_a_log_that_select_events_without_context()
+        public void WithEventsDroppedBySourceContext_should_return_a_log_that_selects_events_without_context()
         {
             var filterLog = baseLog.WithEventsDroppedBySourceContext(context);
 
@@ -70,5 +73,62 @@ namespace Vostok.Logging.Abstractions.Tests.Extensions
 
             baseLog.Received(3).Log(@event);
         }
+
+        [Test]
+        public void WithMinimumLevelForSourceContext_should_return_a_log_that_logs_all_events_with_different_contexts()
+        {
+            var filterLog = baseLog.WithMinimumLevelForSourceContext(context, LogLevel.Warn).ForContext(differentContext);
+
+            ShouldBeEnabledFor(filterLog, GetAllLevels());
+        }
+
+        [Test]
+        public void WithMinimumLevelForSourceContext_should_return_a_log_that_logs_all_events_with_given_contexts_and_permitted_levels()
+        {
+            var filterLog1 = baseLog.WithMinimumLevelForSourceContext(context, LogLevel.Warn)
+                .ForContext(context);
+
+            var filterLog2 = baseLog.WithMinimumLevelForSourceContext(context, LogLevel.Warn)
+                .ForContext(differentContext)
+                .ForContext(context)
+                .ForContext(differentContext);
+
+            ShouldBeEnabledFor(filterLog1, LogLevel.Warn, LogLevel.Error, LogLevel.Fatal);
+            ShouldBeEnabledFor(filterLog2, LogLevel.Warn, LogLevel.Error, LogLevel.Fatal);
+
+            ShouldBeDisabledFor(filterLog1, LogLevel.Debug, LogLevel.Info);
+            ShouldBeDisabledFor(filterLog2, LogLevel.Debug, LogLevel.Info);
+        }
+
+        private void ShouldBeEnabledFor(ILog filterLog, params LogLevel[] levels)
+        {
+            foreach (var level in levels)
+            {
+                var newEvent = @event.WithLevel(level);
+
+                baseLog.ClearReceivedCalls();
+
+                filterLog.IsEnabledFor(level).Should().BeTrue();
+                filterLog.Log(newEvent);
+
+                baseLog.Received(1).Log(newEvent);
+            }
+        }
+
+        private void ShouldBeDisabledFor(ILog filterLog, params LogLevel[] levels)
+        {
+            foreach (var level in levels)
+            {
+                baseLog.ClearReceivedCalls();
+
+                filterLog.IsEnabledFor(level).Should().BeFalse();
+                filterLog.Log(@event.WithLevel(level));
+
+                baseLog.ReceivedCalls().Should().BeEmpty();
+            }
+        }
+
+        private static LogLevel[] GetAllLevels()
+            => Enum.GetValues(typeof(LogLevel)).Cast<LogLevel>().ToArray();
     }
 }
--- Vostok.Logging.Abstractions/Extensions/FilterBySourceContextLogExtensions.cs
+++ Vostok.Logging.Abstractions/Extensions/FilterBySourceContextLogExtensions.cs
@@ -34,6 +34,20 @@ namespace Vostok.Logging.Abstractions
         public static ILog WithEventsDroppedBySourceContext<T>([NotNull] this ILog log) =>
             WithEventsDroppedBySourceContext(log, typeof(T).Name);
 
+        /// <summary>
+        /// <para>Returns a wrapper log that drops events with level lesser than <paramref name="minLevel"/> made by log with given <paramref name="context"/> passed to <see cref="ILog.ForContext(string)"/></para>
+        /// </summary>
+        [Pure]
+        public static ILog WithMinimumLevelForSourceContext([NotNull] this ILog log, [NotNull] string context, LogLevel minLevel)
+            => new SourceContextLevelFilterLog(log, context, minLevel);
+
+        /// <summary>
+        /// <para>Returns a wrapper log that drops events with level lesser than <paramref name="minLevel"/> made by log with context equal to the name of <typeparamref name="T"/> passed to <see cref="ILog.ForContext(string)"/></para>
+        /// </summary>
+        [Pure]
+        public static ILog WithMinimumLevelForSourceContext<T>([NotNull] this ILog log, LogLevel minLevel)
+            => new SourceContextLevelFilterLog(log, typeof(T).Name, minLevel);
+
         private class SourceContextFilterLog : ILog
         {
             private readonly ILog baseLog;
@@ -75,6 +89,46 @@ namespace Vostok.Logging.Abstractions
                 return ReferenceEquals(baseLogForContext, baseLog)
                     ? this
                     : new SourceContextFilterLog(baseLogForContext, contextFilterValue, filterAllowsEvent, logEnabled);
+            }
+        }
+
+        private class SourceContextLevelFilterLog : ILog
+        {
+            private readonly ILog baseLog;
+            private readonly string contextFilterValue;
+            private readonly LogLevel minimumContextLevel;
+            private readonly LogLevel minimumEffectiveLevel;
+
+            public SourceContextLevelFilterLog(ILog baseLog, string contextFilterValue, LogLevel minimumContextLevel)
+                : this(baseLog, contextFilterValue, minimumContextLevel, LogLevel.Debug) { }
+
+            private SourceContextLevelFilterLog(ILog baseLog, string contextFilterValue, LogLevel minimumContextLevel, LogLevel minimumEffectiveLevel)
+            {
+                this.baseLog = baseLog;
+                this.contextFilterValue = contextFilterValue;
+                this.minimumContextLevel = minimumContextLevel;
+                this.minimumEffectiveLevel = minimumEffectiveLevel;
+            }
+
+            public void Log(LogEvent @event)
+            {
+                if (@event?.Level >= minimumEffectiveLevel)
+                    baseLog.Log(@event);
+            }
+
+            public bool IsEnabledFor(LogLevel level) 
+                => level >= minimumEffectiveLevel && baseLog.IsEnabledFor(level);
+
+            public ILog ForContext(string context)
+            {
+                var baseLogForContext = baseLog.ForContext(context);
+
+                if (context == contextFilterValue)
+                    return new SourceContextLevelFilterLog(baseLogForContext, contextFilterValue, minimumContextLevel, minimumContextLevel);
+
+                return ReferenceEquals(baseLogForContext, baseLog)
+                    ? this
+                    : new SourceContextLevelFilterLog(baseLogForContext, contextFilterValue, minimumContextLevel, minimumEffectiveLevel);
             }
         }
     }
