c6c707d1f24fef85597b5489c5cfb65b6b4693b1
iloktionov <iloktionov@skbkontur.ru>
2020-06-25 00:30:26 +0500

    Unroll WithProperties call chain. Also it's now possible to override base log properties in decorators.

---
+++ Vostok.Logging.Abstractions/Extensions/LogPropertiesExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Vostok.Commons.Collections;
+using Vostok.Commons.Formatting;
+
+namespace Vostok.Logging.Abstractions
+{
+    internal static class LogPropertiesExtensions
+    {
+        public static ImmutableArrayDictionary<string, object> WithObjectProperties<T>(
+            this ImmutableArrayDictionary<string, object> properties,
+            T @object,
+            bool allowOverwrite,
+            bool allowNullValues)
+        {
+            if (@object == null)
+                return properties;
+
+            var pairs = @object is IReadOnlyDictionary<string, object> dictionary
+                ? dictionary.Select(pair => (pair.Key, pair.Value))
+                : ObjectPropertiesExtractor.ExtractProperties(@object);
+
+            foreach (var (name, value) in pairs)
+            {
+                if (!allowNullValues && value == null)
+                    continue;
+
+                properties = properties.Set(name, value, allowOverwrite);
+            }
+
+            return properties;
+        }
+    }
+}
