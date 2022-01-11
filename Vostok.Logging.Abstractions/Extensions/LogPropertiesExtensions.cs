51b48ed8926568b66d6b0817a8a63e625932365c
deniaa <deniaa@skbkontur.ru>
2022-01-11 15:08:57 +0500

    Code repair after accepting request https://github.com/vostok/commons.collections/commit/dc47bfcdca223cfd87d42f421fa514c8acf1817d

--- Vostok.Logging.Abstractions/Extensions/LogPropertiesExtensions.cs
+++ Vostok.Logging.Abstractions/Extensions/LogPropertiesExtensions.cs
@@ -17,33 +17,6 @@ namespace Vostok.Logging.Abstractions
             if (@object == null)
                 return properties;
 
-            return FillExistingProperties(@object, allowOverwrite, allowNullValues, properties);
-        }
-
-        public static ImmutableArrayDictionary<string, object> GenerateInitialObjectProperties<T>(
-            T @object,
-            bool allowNullValues)
-        {
-            if (@object == null)
-                return null;
-
-            if (allowNullValues)
-            {
-                if (@object is IReadOnlyDictionary<string, object> dictionary)
-                    return LogEvent.CreatePropertiesFromSource(dictionary);
-
-                //(deniaa): Object properties are always unique by design so we can fill immutable array dictionary without worrying about using the ImmutableArrayDictionary.Set method and overwrite flag.
-                var (count, pairs) = ObjectPropertiesExtractor.ExtractPropertiesWithCount(@object);
-                return LogEvent.CreatePropertiesFromSource(Math.Max(4, count), count, pairs);
-            }
-
-            var properties = LogEvent.CreateProperties();
-
-            return FillExistingProperties(@object, true, false, properties);
-        }
-
-        private static ImmutableArrayDictionary<string, object> FillExistingProperties<T>(T @object, bool allowOverwrite, bool allowNullValues, ImmutableArrayDictionary<string, object> properties)
-        {
             var pairs = @object is IReadOnlyDictionary<string, object> dictionary
                 ? dictionary.Select(pair => (pair.Key, pair.Value))
                 : ObjectPropertiesExtractor.ExtractProperties(@object);
@@ -57,6 +30,41 @@ namespace Vostok.Logging.Abstractions
             }
 
             return properties;
+        }
+
+        public static ImmutableArrayDictionary<string, object> GenerateInitialObjectProperties<T>(
+            T @object,
+            bool allowNullValues)
+        {
+            if (@object == null)
+                return null;
+
+            if (@object is IReadOnlyDictionary<string, object> dictionary)
+            {
+                var properties = LogEvent.CreateProperties(dictionary.Count);
+                foreach (var o in dictionary)
+                {
+                    if (!allowNullValues && o.Value == null)
+                        continue;
+
+                    //(deniaa): We don't know anything about keys comparer in this dictionary so we need to check keys uniqueness.
+                    properties.SetUnsafe(o.Key, o.Value, false);
+                }
+
+                return properties;
+            }
+
+            //(deniaa): Object properties are always unique by design so we can fill immutable array dictionary without worrying about using the ImmutableArrayDictionary.Set method and overwrite flag.
+            var (count, pairs) = ObjectPropertiesExtractor.ExtractPropertiesWithCount(@object);
+            var emptyProperties = LogEvent.CreateProperties(Math.Max(4, count));
+            foreach (var (key, value) in pairs)
+            {
+                if (!allowNullValues && value == null)
+                    continue;
+
+                emptyProperties.AppendUnsafe(key, value);
+            }
+            return emptyProperties;
         }
     }
 }
