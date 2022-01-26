f363dc2747bfb5c9c7a0e3e8f3948abba512362b
kungurtsev <kungurtsev@skbkontur.ru>
2022-01-26 14:14:22 +0500

    add results

--- Vostok.Logging.Abstractions.Tests/Helpers/InterpolatedHelper_Benchmarks.cs
+++ Vostok.Logging.Abstractions.Tests/Helpers/InterpolatedHelper_Benchmarks.cs
@@ -34,7 +34,17 @@ namespace Vostok.Logging.Abstractions.Tests.Helpers
         }
 
 /*
+BenchmarkDotNet=v0.13.0, OS=Windows 10.0.19043.1466 (21H1/May2021Update)
+Intel Core i7-6700 CPU 3.40GHz (Skylake), 1 CPU, 8 logical and 4 physical cores
+.NET SDK=6.0.101
+  [Host]     : .NET 6.0.1 (6.0.121.56705), X64 RyuJIT
+  DefaultJob : .NET 6.0.1 (6.0.121.56705), X64 RyuJIT
 
+
+|  Method |      Mean |    Error |   StdDev |  Gen 0 | Gen 1 | Gen 2 | Allocated |
+|-------- |----------:|---------:|---------:|-------:|------:|------:|----------:|
+|    Same |  61.58 ns | 2.123 ns | 6.259 ns |      - |     - |     - |         - |
+| Escaped | 109.87 ns | 2.288 ns | 5.782 ns | 0.0210 |     - |     - |      88 B |
  */
     }
 }
